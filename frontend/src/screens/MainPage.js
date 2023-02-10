fdb60bc6f030f221cf6ef1f198ef62f14f261e24
mayuri0723 <mayurichavan66@gmail.com>
2023-02-10 17:40:53 +0530

    "Login Page"

--- frontend/src/screens/MainPage.js
+++ frontend/src/screens/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import '../mainpage.css'
-import { Form } from 'react-bootstrap'
+import { Form, Card, Row, Col, Container } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 // Redux Actions
 import { login } from '../actions/userActions'
@@ -20,7 +20,7 @@ const MainPage = ({ location, history }) => {
     dispatch(login(email, password))
     toast.success('Login Successfully!', {
       position: toast.POSITION.TOP_CENTER
-  });
+    });
     localStorage.setItem('isLogin', true)
 
     history.push('/home');
@@ -28,7 +28,77 @@ const MainPage = ({ location, history }) => {
   }
   return (
     <div>
-      MainPage
+      {/* <Container>
+        <Row>
+          <Col sm={8}>
+            <div class="row">
+              <div class="col s12 m8 l4 offset-m2 offset-l4">
+
+                <div class="card">
+
+                  <div class="card-action teal lighten-1 white-text">
+                    <h3>Login Form</h3>
+                  </div>
+
+                  <div class="card-content">
+                    <div class="form-field">
+                      <label for="username">Username</label>
+                      <input type="text" id="username" />
+                    </div><br />
+
+                    <div class="form-field">
+                      <label for="password">Password</label>
+                      <input type="password" id="password" />
+                    </div><br />
+
+
+
+                    <div class="form-field">
+                      <button class="btn-large waves-effect waves-dark" style={{ width: "100px" }}>Login</button>
+                    </div><br />
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </Col>
+
+        </Row>
+
+      </Container> */}
+      {/* <div class="row">
+        <div class="col s12 m8 l4 offset-m2 offset-l4">
+
+          <div class="card">
+
+            <div class="card-action teal lighten-1 white-text">
+              <h3>Login Form</h3>
+            </div>
+
+            <div class="card-content">
+              <div class="form-field">
+                <label for="username">Username</label>
+                <input type="text" id="username" />
+              </div><br />
+
+              <div class="form-field">
+                <label for="password">Password</label>
+                <input type="password" id="password" />
+              </div><br />
+
+
+
+              <div class="form-field">
+                <button class="btn-large waves-effect waves-dark" style={{ width: "100px" }}>Login</button>
+              </div><br />
+            </div>
+
+          </div>
+        </div>
+      </div> */}
+
+
+
       <section className="vh-100" >
         <div className="container  h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
@@ -59,8 +129,7 @@ const MainPage = ({ location, history }) => {
                               margin: "-26px 61px"
                             }}
                           />
-                          {/* <i className="fas fa-cubes fa-2x me-3" style={{color: "#ff6219"}}></i> */}
-                          {/* <span className="h1 fw-bold mb-0">Logo</span> */}
+                         
                         </div>
 
                         <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Sign into your account</h5>
@@ -84,11 +153,7 @@ const MainPage = ({ location, history }) => {
                           <ToastContainer />
                         </div>
 
-                        {/* <a className="small text-muted" href="#">Forgot password?</a>
-                        <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>Don't have an account?
-                          <a href="/Role"
-                            style={{ color: "#393f81" }}>Register here</a>
-                        </p> */}
+                      
                       </Form>
                     </div>
                   </div>
