bd62876a7a35cf928acb886f8daab4d390257f90
mayuri0723 <mayurichavan66@gmail.com>
2023-04-06 14:25:38 +0530

    "Test of APK V.1"

---
+++ frontend/build/precache-manifest.e5281722735ee07d7113ee4e4236173f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56dc77c7ac53b725b6d68f363043fe1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ec3ff35feacc8990cd",
+    "url": "/static/css/2.25629ef7.chunk.css"
+  },
+  {
+    "revision": "b546a1b731d4a1da16cf",
+    "url": "/static/css/main.2ed589ef.chunk.css"
+  },
+  {
+    "revision": "78ec3ff35feacc8990cd",
+    "url": "/static/js/2.665627f2.chunk.js"
+  },
+  {
+    "revision": "55cbfcbda21a2fa187cee680a4f7d5c2",
+    "url": "/static/js/2.665627f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7be2c1128de3326469",
+    "url": "/static/js/3.479ecad1.chunk.js"
+  },
+  {
+    "revision": "6e31a0db202a37e424180fec8d0f2d9a",
+    "url": "/static/js/3.479ecad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258581dc764f2306a180",
+    "url": "/static/js/4.03c8c390.chunk.js"
+  },
+  {
+    "revision": "a4186658d6f60ad1e965921a96caaef2",
+    "url": "/static/js/4.03c8c390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ec6081a4060932112e",
+    "url": "/static/js/5.93da4df2.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/static/js/5.93da4df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b546a1b731d4a1da16cf",
+    "url": "/static/js/main.e5013c01.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e5013c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b3bb1899392e19cf4f",
+    "url": "/static/js/runtime-main.9c9adade.js"
+  }
+]);
